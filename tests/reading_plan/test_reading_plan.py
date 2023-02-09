d58097ab89e25045457b69ff60a56f6e3937d474
ViParis0 <xxvi.paris@gmail.com>
2023-02-09 18:15:51 -0300

    test: add test of function plan_group_news

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,27 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import (
+    ReadingPlanService,
+)  # noqa: F401, E261, E501
+from unittest.mock import MagicMock
+import json
+import pytest
 
 
 def test_reading_plan_group_news():
-    pass
+    ReadingPlanService._db_news_proxy = MagicMock(
+        return_value=[{"reading_time": 10, "title": "noticia incrivel de 10m"}]
+    )
+    readable_news = ReadingPlanService.group_news_for_available_time(10)
+    assert readable_news["unreadable"] == []
+
+    with open("tests/assets/cached_news.json") as f:
+        data = json.load(f)
+        ReadingPlanService._db_news_proxy = MagicMock(return_value=data)
+        readable_news = ReadingPlanService.group_news_for_available_time(10)
+
+        assert "readable" in readable_news
+        assert "unreadable" in readable_news
+        assert readable_news["unreadable"]
+        assert readable_news["readable"][0]["unfilled_time"] == 1
+
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(0)
