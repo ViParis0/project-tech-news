a1d93aa4ed75dabc1574d9cd4ef98dee045e51fe
ViParis0 <xxvi.paris@gmail.com>
2023-02-09 19:29:09 -0300

    feat: add function to search news in db by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 # Requisito 7
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title: str):
@@ -9,7 +10,17 @@ def search_by_title(title: str):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        result = search_news(
+            {
+                "timestamp": datetime.strptime(date, "%Y-%m-%d").strftime(
+                    "%d/%m/%Y"
+                )
+            }
+        )
+        return [(x["title"], x["url"]) for x in result if x]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
