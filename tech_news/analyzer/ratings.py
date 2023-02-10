576984ad7b4b1f7fa6d91769e67556ec1be7b19e
ViParis0 <xxvi.paris@gmail.com>
2023-02-09 21:31:14 -0300

    feat: add function to get top 5 more common categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,20 @@
 # Requisito 10
+from pymongo import MongoClient
+from decouple import config
+from collections import Counter
+
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", default="27017")
+
+client = MongoClient(host=DB_HOST, port=int(DB_PORT))
+db = client.tech_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    results = db.news.find({}, {"category": 1, "_id": 0})
+    categories = [result["category"] for result in list(results)]
+    category_counts = Counter(categories)
+    top_5 = sorted(
+        category_counts.most_common(5), key=lambda x: (-x[1], x[0])
+    )
+    return [x[0] for x in top_5]
